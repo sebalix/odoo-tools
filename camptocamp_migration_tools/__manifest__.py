f77ed5e50e83c7f0a05255084f1346ca394b5da9
Juliette Blanc <jblanc@wrk72.wrk.cby.camptocamp.com>
2023-03-07 09:14:11 +0100

    BSRD-626 : Add camptocamp_migration_tools module in odoo-tools

---
+++ camptocamp_migration_tools/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Camptocamp Migration tools",
+    "version": "15.0.1.0.0",
+    "author": "Camptocamp",
+    "license": "AGPL-3",
+    "category": "Others",
+    "depends": [
+        "base",
+    ],
+    "external_dependencies": {},
+    "website": "http://www.camptocamp.com",
+    "data": [],
+    "installable": True,
+    "auto_install": True,
+}
