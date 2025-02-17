d72e5d6f1d23e65fc6600458fb4bf60cdff3c529
Sébastien Alix <sebastien.alix@camptocamp.com>
2025-02-17 15:56:35 +0100

    camptocamp_migration_tools: fix 'active_backup' field

--- camptocamp_migration_tools/models/ir_ui_view.py
+++ camptocamp_migration_tools/models/ir_ui_view.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class IrUiView(models.Model):
@@ -9,7 +9,14 @@ class IrUiView(models.Model):
 
     active_backup = fields.Boolean(
         string="Active (backup)",
-        related="active",
+        # NOTE: do not use 'related="active"' on purpose. Having a compute
+        # method ensures 'active_backup' field is synced with 'active' field only
+        # on environments having the module code available, so not on Odoo S.A.
+        # Upgrade platform.
+        # Indeed related fields can be computed directly by Odoo even if current
+        # module is not available on the system (i.e. when sending the DB for
+        # migration to Odoo S.A. Upgrade service).
+        compute="_compute_active_backup",
         store=True,
         help=(
             "Store the value of 'active' field to help on the migration.\n"
@@ -18,3 +25,8 @@ class IrUiView(models.Model):
             "values at the end of our own migration."
         ),
     )
+
+    @api.depends("active")
+    def _compute_active_backup(self):
+        for rec in self:
+            rec.active_backup = rec.active
